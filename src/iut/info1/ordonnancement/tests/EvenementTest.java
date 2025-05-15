5cb5dce1aeaa289b9e9fd1a341f42973d7cee60f
Gabriel Robache <gabriel.robache@gmail.com>
2025-05-15 09:41:25 +0200

    Ajout du package de test et de la classe EvenementTest

---
+++ src/iut/info1/ordonnancement/tests/EvenementTest.java
@@ -0,0 +1,75 @@
+/*
+ * EvenementTest.java                                               14 mai 2025
+ * IUT de Rodez, Info 1 2024 - 2025 TP2, pas de copyright
+ */
+package iut.info1.ordonnancement.tests;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * TODO commenter la responsabilité de cette classe (SRP)
+ */
+class EvenementTest {
+
+	@Test
+	void testEvenement() {
+		// TODO Auto-generated method stub
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	void testGetId() {
+		// TODO Auto-generated method stub
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	void testGetTacheAuPlusTot() {
+		// TODO Auto-generated method stub
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	void testGetTacheAuPlusTard() {
+		// TODO Auto-generated method stub
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	void testGetEvenementPredecesseur() {
+		// TODO Auto-generated method stub
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	void testGetEvent_initial() {
+		// TODO Auto-generated method stub
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	void testToString() {
+		// TODO Auto-generated method stub
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	void testCompareTo() {
+		// TODO Auto-generated method stub
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	void testEquals() {
+		// TODO Auto-generated method stub
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	void testHashCode() {
+		// TODO Auto-generated method stub
+		fail("Not yet implemented");
+	}
+}
