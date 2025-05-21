13038cd54797608638e5e1a3a9e0b4a1e4ea1520
Sauvaire Léo <leo.sauvaire@iut-rodez.fr>
2025-05-21 16:27:28 +0200

    getters et setters chargeurCSV

--- src/iut/info1/ordonnancement/ChargeurCSV.java
+++ src/iut/info1/ordonnancement/ChargeurCSV.java
@@ -20,7 +20,13 @@ import java.util.Scanner;
  * @version 1.0
  */
 public class ChargeurCSV {
-
+	
+	/* Chemin du fichier CSV à écrire */
+	private static String cheminFichierEcrit;
+	
+	/* Chemin du fichier CSV à lire */
+	private static String cheminFichierLu;
+	
     /**
      * Enregistre des données dans un fichier CSV avec des valeurs
      * séparées par des points-virgules.
@@ -28,11 +34,11 @@ public class ChargeurCSV {
      * @param donnees une liste de lignes, où chaque ligne est un
      * tableau de chaînes.
      */
-    public static void ecrireCSV(String cheminFichier, 
-                                 ArrayList<String[]> donnees) {
+    public static void ecrireCSV(String cheminFichierEcriture, ArrayList<String[]> donnees) {
+    	
+    	cheminFichierEcrit = cheminFichierEcriture;    	
         try {
-            BufferedWriter ecriture =
-                    new BufferedWriter(new FileWriter(cheminFichier));
+            BufferedWriter ecriture = new BufferedWriter(new FileWriter(cheminFichierEcriture));
 
             for (String[] ligne : donnees) {
                 for (int i = 0; i < ligne.length; i++) {
@@ -61,6 +67,9 @@ public class ChargeurCSV {
      */
     public static void lireCSV(String cheminFichier,
                                ArrayList<String[]> donnees) {
+    	
+    	cheminFichierLu = cheminFichier;
+    	
         try {
             BufferedReader lecture =
                         new BufferedReader(new FileReader(cheminFichier));
@@ -78,4 +87,80 @@ public class ChargeurCSV {
                               + erreurLecture.getMessage());
         }
     }
+    
+	/**
+	 * Tests temporaires de la classe ChargeurCSV
+	 */
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // Exemple de données qui vont être écrites dans le fichier CSV
+        ArrayList<String[]> nouvellesDonnees = new ArrayList<>();
+        nouvellesDonnees.add(new String[] {"EtapePrecedente", "Tache", "EtapeSuivante"});
+        nouvellesDonnees.add(new String[] {"0", "a", "2"});
+        nouvellesDonnees.add(new String[] {"1", "c", "3"});
+        nouvellesDonnees.add(new String[] {"2", "j", "3"});
+
+        /* Saisie du chemin d’écriture du fichier CSV */
+        System.out.print("Entrez le chemin du fichier CSV dans lequel écrire les données "
+        		           + "(les données présentes dans ce fichier seront écrasées) : ");
+        String cheminEcriture = scanner.nextLine();
+        ecrireCSV(cheminEcriture, nouvellesDonnees);
+        
+        // Saisie du chemin de lecture du fichier CSV
+        System.out.print("\nEntrez le chemin du fichier CSV à lire : ");
+        String cheminLecture = scanner.nextLine();
+        
+        // Lecture du fichier CSV
+        ArrayList<String[]> donneesLues = new ArrayList<>();
+        lireCSV(cheminLecture, donneesLues);
+        System.out.println("Données lues :\n");
+		
+        // Affichage des données lues
+        for (int nbValeurs = 0; nbValeurs < donneesLues.size(); nbValeurs++) {
+			String[] ligne = donneesLues.get(nbValeurs);
+			for (int i = 0; i < ligne.length; i++) {
+				System.out.print(ligne[i]);
+				if (i < ligne.length - 1) {
+					System.out.print(";");
+				}
+			}
+			System.out.println();
+		}
+
+        scanner.close();
+    }
+    
+	/*
+	 * Renvoie le chemin du fichier CSV à écrire
+	 * @return le chemin du fichier CSV à écrire
+	 */
+    public String getCheminFichier() {
+        return cheminFichierEcrit;
+    }
+    
+	/*
+	 * Renvoie le chemin du fichier CSV à lire
+	 * @return le chemin du fichier CSV à lire
+	 */
+	public String getCheminFichierLecture() {
+        return cheminFichierLu;
+	}
+	
+	/**
+	 * Modifie le chemin du fichier CSV à écrire
+	 * @param nouveauCheminFichierEcrit le nouveau chemin du fichier CSV à écrire
+	 */
+	public void setCheminFichierEcrit(String nouveauCheminFichierEcrit) {
+		cheminFichierEcrit = nouveauCheminFichierEcrit;
+	}
+	
+	/**
+	 * Modifie le chemin du fichier CSV à lire
+	 * @param nouveauCheminFichierLu le nouveau chemin du fichier CSV à lire
+	 */
+	public void setCheminFichierLu(String nouveauCheminFichierLu) {
+		cheminFichierLu = nouveauCheminFichierLu;
+	}
 }
+
